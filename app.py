ca8fd7c17161b4021fb5b7030d68ebb0b36739de
Thilac <163951647+Thilac01@users.noreply.github.com>
2024-03-30 08:30:08 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import pandas as pd
-
+import time
 
 df = pd.read_csv('data.csv')
 
@@ -15,28 +15,38 @@ c3 = 0.0
 
 m = len(X)
 
-for _ in range(iterations):
+print("Training the model...")
+
+for epoch in range(iterations):
     c1_gradient = 0
     c2_gradient = 0
     c3_gradient = 0
-    
+
     for i in range(m):
         x1 = X.iloc[i]['Age']
         x2 = X.iloc[i]['BloodPressure']
         x3 = X.iloc[i]['Insulin']
 
-        y_pred = c1 * x1 + c2 * x2 + c3*x3
+        y_pred = c1 * x1 + c2 * x2 + c3 * x3
 
         c1_gradient += (1/m) * x1 * (y_pred - y.iloc[i])
         c2_gradient += (1/m) * x2 * (y_pred - y.iloc[i])
         c3_gradient += (1/m) * x3 * (y_pred - y.iloc[i])
-    
+
     c1 -= alpha * c1_gradient
     c2 -= alpha * c2_gradient
     c3 -= alpha * c3_gradient
 
+    
+    print(f"\rEpoch {epoch+1}/{iterations} {'▋'*(epoch+1)}", end='', flush=True)
+    time.sleep(0.1) 
+
+print("\nTraining completed.")
+
 age = 43
 blood_pressure = 120
-Insuline = 20
-prediction = c1 * age + c2 * blood_pressure + c3 * Insuline
+insulin = 20  
+
+
+prediction = c1 * age + c2 * blood_pressure + c3 * insulin  
 print("Predicted diabetes percentage:", prediction)
